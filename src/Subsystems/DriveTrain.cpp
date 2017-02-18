2353a9fef7b4df877284f8e11c6f88b9959e121b
Phan-2k2 <austinphan2k2@gmail.com>
2017-02-18 11:31:32 -0800

    Add files via upload
    Fixed turning

--- src/Subsystems/DriveTrain.cpp
+++ src/Subsystems/DriveTrain.cpp
@@ -12,10 +12,10 @@ DriveTrain::DriveTrain()
 	robotDrive = new RobotDrive(F_R_Motor, B_R_Motor, F_L_Motor, B_L_Motor);
 //	robotDrive -> SetSensitivity(0.75);
 //	robotDrive -> SetMaxOutput(double(Stick->GetZ()));
-	robotDrive->SetInvertedMotor(RobotDrive::kFrontRightMotor, false);
-	robotDrive->SetInvertedMotor(RobotDrive::kRearRightMotor, false);
-	robotDrive->SetInvertedMotor(RobotDrive::kFrontLeftMotor, false);
-	robotDrive->SetInvertedMotor(RobotDrive::kRearLeftMotor, false);
+//	robotDrive->SetInvertedMotor(RobotDrive::kFrontRightMotor, false);
+//	robotDrive->SetInvertedMotor(RobotDrive::kRearRightMotor, false);
+//	robotDrive->SetInvertedMotor(RobotDrive::kFrontLeftMotor, false);
+//	robotDrive->SetInvertedMotor(RobotDrive::kRearLeftMotor, false);
 
 	encoder = new Encoder(Encoder_Pos, Encoder_Neg, false, Encoder::EncodingType::k4X);
 	encoder->SetMaxPeriod(0.1);
@@ -41,7 +41,7 @@ void DriveTrain::DriveWithJoystick(Joystick* Stick){
 	double max_speed = 1.0 - (Stick->GetZ()*50+50)*(0.01);
 	robotDrive -> SetMaxOutput(max_speed);
 
-	robotDrive->ArcadeDrive(Stick);
+	robotDrive->ArcadeDrive(Stick->GetY(), Stick->GetX()*(-1));
 	SmartDashboard::PutNumber("MaxOutput", max_speed);
 	SmartDashboard::PutNumber("Encoder", encoder->GetDistance());
 	SmartDashboard::PutNumber("Get Angle", gyro->GetAngle());
